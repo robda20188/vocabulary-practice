7bdb20fc1c0148f8bff7d1d1703beb1baa0c1107
robda20188 <danielrouco188rr@gmail.com>
2024-03-22 21:20:23 +0100

    Basic HTML structure and CSS

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("crear").addEventListener("click", () => {
-    document.getElementById("formulario").style.display = "flex";
-    document.getElementById("inicio").style.display = "none";
-})
-
-document.getElementById("titulo").addEventListener("click", () => {
-    document.getElementById("formulario").style.display = "none";
-    document.getElementById("inicio").style.display = "flex";
-})
