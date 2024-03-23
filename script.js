f45cb7e50e03d1700e73a562420946aec28c1f6f
robda20188 <danielrouco188rr@gmail.com>
2024-03-23 16:53:09 +0100

    Adding trim()

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ const nameInput = document.getElementById("name-input");
 const pasteInput = document.getElementById("paste-input");
 
 cretaListBtn.addEventListener("click", function(){
-    let words = pasteInput.value.replace(" ", "").split(",");
+    let words = pasteInput.value.split(",");
 
     for(let i = 0; i < words.length; i++){
         words[i] = words[i].split(":");
-        words[i] = new Word(words[i][0], words[i][1]);
+        words[i] = new Word(words[i][0].trim(), words[i][1].trim());
     }
 
     lists.push(new List(nameInput.value, words));
